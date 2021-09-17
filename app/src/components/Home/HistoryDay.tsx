8dc84d5aa36fd87615c21131882dc8d9ca45ea79
brkss <berkassebrahim@gmail.com>
2021-09-17 18:56:59 +0100

    📦 Dynamic Day component for history

---
+++ app/src/components/Home/HistoryDay.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface Props {
+  day: string;
+  month: string;
+}
+
+export const HistoryDay: React.FC<Props> = ({ day, month }) => {
+  return (
+    <View style={style.day}>
+      <Text style={style.dayNumber}>{day}</Text>
+      <Text style={style.monthName}>{month}.</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  day: {
+    marginRight: 8,
+    backgroundColor: "#D8D8D8",
+    height: 77,
+    width: 77,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dayNumber: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  monthName: {
+    fontSize: 12,
+    marginTop: -2,
+  },
+});
--- app/src/components/Home/HistoryHome.tsx
+++ app/src/components/Home/HistoryHome.tsx
@@ -1,15 +1,51 @@
 import React from "react";
 import { Text, StyleSheet, View, ScrollView } from "react-native";
+import { HistoryDay } from "./HistoryDay";
+
+const days = [
+  {
+    day: "09",
+    month: "Sep",
+  },
+  {
+    day: "08",
+    month: "Sep",
+  },
+  {
+    day: "07",
+    month: "Sep",
+  },
+  {
+    day: "06",
+    month: "Sep",
+  },
+  {
+    day: "05",
+    month: "Sep",
+  },
+  {
+    day: "04",
+    month: "Sep",
+  },
+
+  {
+    day: "03",
+    month: "Sep",
+  },
+];
 
 export const HistoryHome: React.FC = () => {
   return (
     <View style={style.container}>
       <Text style={style.title}>History.</Text>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <View style={style.day}>
-          <Text style={style.dayNumber}>09</Text>
-          <Text style={style.monthName}>Sep.</Text>
-        </View>
+      <ScrollView
+        horizontal={true}
+        style={{ marginTop: 15 }}
+        showsHorizontalScrollIndicator={false}
+      >
+        {days.map((day) => (
+          <HistoryDay {...day} />
+        ))}
       </ScrollView>
     </View>
   );
@@ -22,21 +58,5 @@ const style = StyleSheet.create({
   title: {
     fontSize: 30,
     fontWeight: "bold",
-  },
-  day: {
-    backgroundColor: "#D8D8D8",
-    height: 77,
-    width: 77,
-    borderRadius: 100,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dayNumber: {
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  monthName: {
-    fontSize: 12,
-    marginTop: -2,
   },
 });
