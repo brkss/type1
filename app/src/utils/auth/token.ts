e7eb4b6de5d38e1d4a77db4eac3494dac1487943
brkss <berkassebrahim@gmail.com>
2021-09-21 03:13:31 +0100

    📦 Using context and reducer for auth state management

--- app/src/utils/auth/token.ts
+++ app/src/utils/auth/token.ts
@@ -1,12 +1,3 @@
 import React from "react";
 
-export const _token = React.createContext({ token: "" });
-
-export const setToken = (_token: string): void => {
-  //token = _token;
-};
-
-export const getToken = (): string => {
-  //return token;
-  return "";
-};
+export const token = React.createContext<string | null>(null);
