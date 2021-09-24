79a1340707f200b63de85ff1190382581d50c3eb
brkss <berkassebrahim@gmail.com>
2021-09-24 21:42:23 +0100

    ⚡️ Config Demon : UI

--- app/src/screens/Demon/ConfigureDemon.tsx
+++ app/src/screens/Demon/ConfigureDemon.tsx
@@ -4,11 +4,38 @@ import { View, Text, StyleSheet } from "react-native";
 export const ConfigureDemon: React.FC<any> = ({ route }) => {
   return (
     <View style={style.container}>
-      <Text>{route.params.name}</Text>
+      <View style={style.wrap}>
+        <View style={style.demonInfo}>
+          <Text style={style.demonImage}>👹</Text>
+          <Text style={style.demonName}>{route.params.name} Demon.</Text>
+          <Text style={style.demonDescription}>
+            This demon job is keep tracking your meals schedule.
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
-  container: {},
+  container: {
+    marginTop: 50,
+  },
+  wrap: {
+    padding: 10,
+  },
+  demonInfo: {
+    //alignItems: "center",
+  },
+  demonImage: {
+    fontSize: 100,
+    textAlign: "center",
+  },
+  demonName: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  demonDescription: {
+    marginTop: 5,
+  },
 });
