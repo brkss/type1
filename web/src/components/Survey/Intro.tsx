3a4e1f295529fe9638497e11a0d51f41e346326e
brkss <berkassebrahim@gmail.com>
2021-09-29 15:59:44 +0100

    ⛹️‍♂️ Survey Intro : Animation

--- web/src/components/Survey/Intro.tsx
+++ web/src/components/Survey/Intro.tsx
@@ -3,9 +3,18 @@ import { Heading, Box } from "@chakra-ui/react";
 import { gsap } from "gsap";
 import styled from "@emotion/styled";
 
-export const SurveyIntro: React.FC = () => {
+interface Props {
+  onFinish: () => void;
+}
+
+export const SurveyIntro: React.FC<Props> = ({ onFinish }) => {
   const headingRef = React.useRef();
+  const buttonRef = React.useRef();
   const [showButtom, SetShowButton] = React.useState<boolean>(true);
+  React.useEffect(() => {
+    gsap.to(buttonRef.current as any, { opacity: 1, duration: 2, delay: 1 });
+    gsap.to(headingRef.current as any, { opacity: 1, y: "-=20", duration: 2 });
+  }, []);
   const start = () => {
     SetShowButton(false);
     gsap.to(headingRef.current as any, {
@@ -13,16 +22,21 @@ export const SurveyIntro: React.FC = () => {
       duration: 3,
       onComplete: () => {
         console.log("log");
+        onFinish();
       },
     });
   };
 
   return (
     <Box>
-      <Heading fontSize={"50px"} ref={headingRef as any}>
+      <Heading opacity={0} y={40} fontSize={"50px"} ref={headingRef as any}>
         Survey
       </Heading>
-      {showButtom ? <Button onClick={() => start()}>Start</Button> : null}
+      {showButtom ? (
+        <Button ref={buttonRef as any} onClick={() => start()}>
+          Start
+        </Button>
+      ) : null}
     </Box>
   );
 };
@@ -35,4 +49,5 @@ const Button = styled.button`
   color: white;
   font-weight: bold;
   display: block;
+  opacity: 0;
 `;
--- web/src/pages/Survey.tsx
+++ web/src/pages/Survey.tsx
@@ -3,10 +3,20 @@ import { Box, Center, Heading } from "@chakra-ui/react";
 import { SurveyIntro } from "../components/Survey";
 
 export const Survey: React.FC = () => {
+  const [current, SetCurrent] = React.useState<number>(1);
+  const finished = () => {
+    SetCurrent(current + 1);
+  };
+
   return (
     <Box overflow={"hidden"} minH={"100vh"}>
       <Center minH={"100vh"} overflow={"hidden"}>
-        <SurveyIntro />
+        {
+          {
+            1: <SurveyIntro onFinish={() => finished()} />,
+            2: <Heading>Hello !</Heading>,
+          }[current]
+        }
       </Center>
     </Box>
   );
